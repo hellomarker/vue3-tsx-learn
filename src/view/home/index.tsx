422a2c96bf22675bc6444acd7c3c32c5d3689b8d
yangtian <yangtian@wimift.com>
2022-08-08 19:06:31 +0800

    add

--- src/view/home/index.tsx
+++ src/view/home/index.tsx
@@ -1,21 +1,26 @@
 import { defineComponent, ref } from "vue";
-import { useDialog } from "@/components/Dialog";
+import { Dialog } from "@/components";
 
 export default defineComponent({
   name: "homePage",
   setup() {
-    const { Dialog, triggerDialog } = useDialog();
+    const visible = ref(false);
+    const triggerDialog = () => {
+      visible.value = !visible.value;
+    };
     return () => (
       <>
         <img src={require("../../assets/logo.png")} alt="" />
         <button onClick={triggerDialog}>弹窗</button>
-        <Dialog>
+        <Dialog visible={visible.value}>
           {{
-            title: "这是标题",
-            default: <div>
-              <del>这是内容~</del>
-              <span>这才是内容</span>
-            </div>,
+            title: () => "这是标题",
+            default: () => (
+              <div>
+                <del>这是内容~</del>
+                <span>这才是内容</span>
+              </div>
+            ),
           }}
         </Dialog>
       </>
